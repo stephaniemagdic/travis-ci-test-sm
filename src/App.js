62b3a2d1211246ec26a26902a911db6cdfa12052
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-08 08:09:47 -0600

    Add text to test auto-redeployment

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          TEST that this is visible for auto redeploy
         </a>
       </header>
     </div>
